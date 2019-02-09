0056fc96e9bab47205b702d7183ef9f526d819d0
DidntRead <koko1999@mail.bg>
2019-02-09 18:05:28 +0200

    remove some unneeded libs

--- k5fpr-vendor-blobs.mk
+++ k5fpr-vendor-blobs.mk
@@ -1,17 +1,3 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/vendor/bin,$(TARGET_COPY_OUT_VENDOR)/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/vendor/lib,$(TARGET_COPY_OUT_VENDOR)/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/vendor/firmware,$(TARGET_COPY_OUT_VENDOR)/firmware)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/vendor/thh,$(TARGET_COPY_OUT_VENDOR)/thh)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/etc,system/etc)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/usr,system/usr)
 
-
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/vendor,$(TARGET_COPY_OUT_VENDOR))
